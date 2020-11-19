26c6de572be5c37529ed4cfbaf47593481cbc8a8
Heo Jong Tae <heo9910@naver.com>
2020-11-19 22:53:08 +0900

    .

--- chapter-01-querydsl-configuration/src/test/java/com/study/querydsl/StartApplicationTests.java
+++ chapter-01-querydsl-configuration/src/test/java/com/study/querydsl/StartApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Chapter01ProjectConfigurationApplicationTests {
+class StartApplicationTests {
 
 	@Test
 	void contextLoads() {
