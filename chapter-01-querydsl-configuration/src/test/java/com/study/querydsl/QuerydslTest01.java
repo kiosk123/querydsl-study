17a3c8d8772ec6f78108f07bdcaf4613cd9e19c7
Heo Jong Tae <heo9910@naver.com>
2020-11-20 01:44:27 +0900

    .

--- chapter-01-querydsl-configuration/src/test/java/com/study/querydsl/QuerydslTest01.java
+++ chapter-01-querydsl-configuration/src/test/java/com/study/querydsl/QuerydslTest01.java
@@ -1,5 +1,7 @@
 package com.study.querydsl;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import javax.persistence.EntityManager;
 import javax.transaction.Transactional;
 
@@ -10,6 +12,7 @@ import org.springframework.test.context.ActiveProfiles;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.study.querydsl.domain.Hello;
+import com.study.querydsl.domain.QHello;
 
 @ActiveProfiles("test")
 @SpringBootTest
@@ -24,9 +27,15 @@ class QuerydslTest01 {
 	    Hello hello = new Hello();
 	    em.persist(hello);
 	    
-	    em.flush();
-	    em.clear();
-	    
 	    JPAQueryFactory query = new JPAQueryFactory(em);
+	    
+	    //생성자 파라미터로 alias가 들어감 JPQL에서 from Member m에서 m역할
+	    QHello qHello = new QHello("h"); 
+	    
+	    Hello result = query.selectFrom(qHello)
+	                        .fetchOne();
+	    
+	    assertEquals(result, hello);
+	    assertEquals(hello.getId(), hello.getId());
 	}
 }
