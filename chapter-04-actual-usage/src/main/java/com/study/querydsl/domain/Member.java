df1e4ad6aa724a3cca9a7f5a77026b9af5ce40de
Heo Jong Tae <heo9910@naver.com>
2020-11-22 01:29:39 +0900

    .

---
+++ chapter-04-actual-usage/src/main/java/com/study/querydsl/domain/Member.java
@@ -0,0 +1,63 @@
+package com.study.querydsl.domain;
+
+import java.util.Objects;
+
+import javax.jdo.annotations.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(of = {"id", "userName", "age"})
+public class Member extends BaseEntity {
+    
+    @Column(name = "MEMBER_ID")
+    @Id @GeneratedValue
+    @Getter
+    private Long id;
+    
+    @Getter @Setter
+    private String userName;
+    
+    @Getter @Setter
+    private Integer age;
+    
+    @ManyToOne(fetch = FetchType.LAZY )
+    @JoinColumn(name = "TEAM_ID")
+    @Getter
+    Team team;
+    
+    
+    public Member(String userName) {
+        this.userName = userName;
+    }
+    
+    public Member(String userName, Integer age) {
+        this(userName);
+        this.age = age;
+    }
+    
+    public Member(String userName, Integer age, Team team) {
+        this(userName, age);
+        this.team = team;
+    }
+    
+    public void changeTeam(Team team) {
+        this.team = team;
+        if (!team.getMembers().isEmpty() && !Objects.isNull(getId())) {
+            team.getMembers().removeIf(m -> getId().equals(m.getId()));
+        }
+        team.getMembers().add(this);
+    }
+}
